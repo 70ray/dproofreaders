4edd1a40d18ee1deacf245e62719e8c31772e660
Ray Papworth <raypapworth@gmail.com>
2019-04-01 06:19:45 +0000

    key titles transferred in header args instead of php/js file

--- tools/proofers/ctrl_frame.php
+++ tools/proofers/ctrl_frame.php
@@ -1,10 +1,11 @@
 <?php
-$relPath="./../../pinc/";
+$relPath="../../pinc/";
 include_once($relPath.'base.inc');
 include_once($relPath.'slim_header.inc');
 include_once($relPath.'stages.inc');
 include_once($relPath.'misc.inc'); // get_enumerated_param()
 include_once($relPath.'ProofreadingToolbox.inc');
+include_once($relPath.'key_titles.inc');
 
 $round_id = get_enumerated_param($_GET, 'round_id', null, array_keys($Round_for_round_id_));
 $round = get_Round_for_round_id($round_id);
@@ -15,12 +16,12 @@ $header_args = [
     ],
     "js_files" => [
         "$code_url/scripts/api.js",
-        "$code_url/scripts/key_titles.php",
         "$code_url/scripts/misc.js",
         "$code_url/scripts/character_selector.js",
     ],
     "js_data" => "
         var apiUrl = '$code_url/api/';
+        var keyTitles = $key_titles;
     ",
 ];
 slim_header(_("Control Frame"), $header_args);
