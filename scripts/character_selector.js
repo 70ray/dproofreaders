43bca61720686511edbc2fe651cdb5c5053fffcc
Ray Papworth <raypapworth@gmail.com>
2019-07-30 11:10:58 +0000

    translation functions removed

--- scripts/character_selector.js
+++ scripts/character_selector.js
@@ -2,41 +2,6 @@
 
 $(function () {
     "use strict";
-    var transTable = {
-        "\u0100": "[=A]",
-        "\u0101": "[=a]",
-        "\u0102": "[)A]",
-        "\u0103": "[)a]",
-        "\u0112": "[=E]",
-        "\u0113": "[=e]",
-        "\u0114": "[)E]",
-        "\u0115": "[)e]",
-        "\u012a": "[=I]",
-        "\u012b": "[=i]",
-        "\u012c": "[)I]",
-        "\u012d": "[)i]",
-        "\u014c": "[=O]",
-        "\u014d": "[=o]",
-        "\u014e": "[)O]",
-        "\u014f": "[)o]",
-        "\u016a": "[=U]",
-        "\u016b": "[=u]",
-        "\u016c": "[)U]",
-        "\u016d": "[)u]",
-    };
-    function translate(ch) {
-        var trans;
-        if (ch.codePointAt(0) < 0x100) {
-            return ch;
-        } else {
-            trans = transTable[ch];
-            if (trans) {
-                return trans;
-            } else {
-                return "?";
-            }
-        }
-    }
 
     var charSelector = $("#char-selector");
     var largeChar = document.getElementById("large_char");
@@ -96,7 +61,7 @@ $(function () {
         });
 
         $(".picker", charSelector).click(function () {
-            top.insertCharacter(translate(this.innerText));
+            top.insertCharacter(this.innerText);
         }).mouseover(function () {
             largeChar.value = this.innerText;
         }).each(function () {
